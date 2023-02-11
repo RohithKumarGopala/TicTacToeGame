80784e40a4cbd8101da06277be13c78e045396a0
“rohithgopala44” <rohithgopala44@gmail.com>
2023-02-11 13:43:29 +0530

    Refactor: Program To Place Character In Board

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,39 +1,61 @@
 package com.tictactoegame;
 import java.util.Scanner;
+
 public class TicTacToe {
     int i;
     static char[] board = new char[10];
     static char player, computer;
-    Scanner scanner = new Scanner(System.in);
-    public void initialize() {
-        for (i = 1; i < 10; i++) {
-            board[i] = ' ';
+    private static int playLocation;
+    public static Scanner scannerObject = new Scanner(System.in);
+    public static void createBoard()
+    {
+        for (int index = 1; index < 10; index++)
+        {
+            board[index] = ' ';
         }
     }
-    public void check() {
-        System.out.println("Please Select Your Choice Letter : 'X' or 'O' ");
-        char choice = scanner.next().charAt(0);
-        if (choice == 'X') {
-            player = 'X';
-            computer = 'O';
-        } else if (choice == 'O') {
-            player = 'O';
-            computer = 'X';
-        }
-    }
-    public void showboard() {
+    public static void getPlayerChoice()
+    {
 
-        System.out.println("Welcome to Tic Tac Toe");
+        System.out.print("select X or O : ");
+        player = scannerObject.next().toUpperCase().charAt(0);
+
+        if (player == 'X')
+            computer = 'O';
+        else
+            computer = 'X';
+        System.out.println("You have selected : " + player);
+        System.out.println("Computer's choice is : " + computer);
+    }
+    public static void showBoard()
+    {
         System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
         System.out.println("---------");
         System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
         System.out.println("---------");
         System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
     }
-    public static void main(String[] args) {
+    public static void userMove()
+    {
+        System.out.println("Enter Location 1-9 to Make Move");
+        playLocation = scannerObject.nextInt();
+        if (playLocation < 10 && playLocation > 0)
+        {
+            board[playLocation] = player;
+            showBoard();
+        } else
+        {
+            System.out.println("Invalid Choice");
+        }
+    }
+    public static void main(String[] args)
+    {
 
-        TicTacToe object = new TicTacToe();
-        object.initialize();
-        object.showboard();
+        System.out.println("Welcome To Tic Tac Toe");
+        createBoard();
+        getPlayerChoice();
+        showBoard();
+        userMove();
+
     }
 }
